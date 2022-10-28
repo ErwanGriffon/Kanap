9c0c6fbac9aca8c4d92aa9e684ff8c4c0849a19b
Erwan Griffon <84179694+ErwanGriffon@users.noreply.github.com>
2022-10-28 15:17:51 +0200

    Good

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,25 +8,3 @@ function getCart() {
   }
 
 // Check local storage product
-function add2Cart(productId, color, qty) {
-    if (qty <= 0 || color == "") {
-      return;
-    }
-    let items = getCart();
-    if (items.length == 0) {
-      items = [[productId, color, qty]];
-    } else {
-      let found = false;
-      for (let i = 0; i < items.length; i++) {
-        if (productId === items[i][0] && color === items[i][1]) {
-          found = true;
-          items[i][2] += qty;
-        }
-      }
-      if (found == false) {
-        let item = [productId, color, qty];
-        items.push(item);
-      }
-    }
-    localStorage.setItem("panier", JSON.stringify(items));
-  }
